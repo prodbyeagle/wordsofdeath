fce2cb40db4359307bd5fe72fba285d8d05ee2c4
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-04 04:25:22 +0100

    - config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,5 +27,6 @@ module.exports = {
          }
       },
    },
-   plugins: [],
+   // eslint-disable-next-line @typescript-eslint/no-require-imports
+   plugins: [require('tailwindcss-motion')],
 };

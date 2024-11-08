3ec0ba92eba27cd927111248b471a0901d031dc3
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-08 18:25:04 +0100

    - font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from '../components/Navbar';
 import { Inter_Tight } from 'next/font/google'
 
-const sg = Inter_Tight({ fallback: ["inter-tight, system-ui"], subsets: ['latin'] })
+const Inter = Inter_Tight({ fallback: ["inter-tight, system-ui"], subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -13,8 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body>
-        className={sg.className}
+      <body className={Inter.className}>
         <Navbar />
         {children}
       </body>

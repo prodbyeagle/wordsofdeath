3ed99f3bdf79deeef1c979c593f2111526c66c15
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-08 18:21:07 +0100

    - updated dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,5 +1,5 @@
 // src/components/Dropdown.tsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface DropdownProps {
    children: React.ReactNode;
@@ -9,10 +9,31 @@ interface DropdownProps {
 }
 
 const Dropdown: React.FC<DropdownProps> = ({ isOpen, toggleDropdown, disabled, children }) => {
-   if (!isOpen || disabled) return null; // Dropdown nicht anzeigen, wenn es geschlossen oder deaktiviert ist
+   const dropdownRef = useRef<HTMLDivElement>(null);
+
+   useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+         if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            toggleDropdown();
+         }
+      };
+
+      if (isOpen && !disabled) {
+         document.addEventListener('mousedown', handleClickOutside);
+      }
+
+      return () => {
+         document.removeEventListener('mousedown', handleClickOutside);
+      };
+   }, [isOpen, disabled, toggleDropdown]);
+
+   if (!isOpen || disabled) return null;
 
    return (
-      <div className="absolute right-0 top-12 mt-2 w-48 bg-transparent backdrop-blur-xl rounded-md shadow-lg z-10 border border-zinc-600">
+      <div
+         ref={dropdownRef}
+         className="absolute right-0 top-12 mt-2 w-48 bg-transparent backdrop-blur-xl rounded-md shadow-lg z-10 border border-zinc-600"
+      >
          <div className="duration-100 transition-all" onClick={toggleDropdown}>
             {children}
          </div>

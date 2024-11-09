5f7f86209ce1f36cae7911b90f244242f23d7ee6
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-09 03:31:19 +0100

    - tailwind ermerm

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,28 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      maxWidth: {
+        120: '430px',
+        150: '480px',
+        200: '550px',
+      },
+      maxHeight: {
+        120: '430px',
+        150: '480px',
+        200: '550px',
+      },
+      boxShadow: {
+        '3xl': '0 10px 30px 0px rgba(0, 0, 0, 0.5)',
+        '4xl': '0 10px 40px 0px rgba(0, 0, 0, 0.5)',
+        '5xl': '0 10px 50px 0px rgba(0, 0, 0, 0.5)',
+        '6xl': '0 15px 60px 0px rgba(0, 0, 0, 0.5)',
+        '7xl': '0 10px 900px 200px rgba(0, 0, 0, 0.5)',
+      },
+      transitionDuration: {
+        2000: '2000ms',
+        3000: '3000ms',
+        4000: '4000ms',
+      }
     },
   },
   plugins: [],

fae2ee8be2a9a1cc015b64caf2828aaec2472a3c
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-15 16:06:27 +0100

    - erm

--- src/app/e/[id]/page.tsx
+++ src/app/e/[id]/page.tsx
@@ -5,6 +5,7 @@ import { Entry, User } from "@/types";
 import { formatDistanceToNow } from "date-fns";
 import { de } from "date-fns/locale";
 import { BadgeCheck, Clock, HeartHandshake, Server } from "lucide-react";
+import Head from "next/head";
 
 interface EntryProps {
     params: Promise<{ id: string }>;
@@ -103,107 +104,118 @@ const EntryPage = async ({ params }: EntryProps) => {
         .slice(0, 3);
 
     return (
-        <div className="min-h-screen bg-zinc-900 text-zinc-200 cursor-default flex items-center justify-center py-12 px-6">
-            <div className="max-w-3xl w-full p-8 rounded-xl shadow-2xl bg-zinc-800">
-                <h2 className="text-3xl font-bold mb-4 break-words">{entry.entry}</h2>
-                <div className="text-sm text-zinc-400 mb-6 flex items-center space-x-1">
-                    <Clock size={16} className="text-zinc-400" />
-                    <span>
-                        {formatDistanceToNow(new Date(entry.timestamp), {
-                            includeSeconds: true,
-                            addSuffix: true,
-                            locale: de,
-                        })}{" "}
-                        erstellt.
-                    </span>
-                </div>
+        <>
+            <Head>
+                <title>{entry.entry} - Words of Death</title>
+                <meta name="description" content={`Eintrag von ${user?.username}: ${entry.entry}`} />
+                <meta name="author" content={user?.username || "Unknown"} />
+                <meta property="og:title" content={entry.entry} />
+                <meta property="og:description" content={`Eintrag von ${user?.username}: ${entry.entry}`} />
+                <meta property="og:image" content={`https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}`} />
+            </Head>
 
-                {entry.variation.length > 0 && (
-                    <div className="mt-6">
-                        <h3 className="text-xl font-semibold mb-3 text-zinc-200">Variationen</h3>
-                        <div className="bg-zinc-700 p-4 rounded-xl">
-                            <ul className="list-disc ml-6 space-y-1">
-                                {entry.variation.map((var1, index) => (
-                                    <li key={index} className="text-zinc-300">
-                                        {var1}
-                                    </li>
+            <div className="min-h-screen bg-zinc-900 text-zinc-200 cursor-default flex items-center justify-center py-12 px-6">
+                <div className="max-w-3xl w-full p-8 rounded-xl shadow-2xl bg-zinc-800">
+                    <h2 className="text-3xl font-bold mb-4 break-words">{entry.entry}</h2>
+                    <div className="text-sm text-zinc-400 mb-6 flex items-center space-x-1">
+                        <Clock size={16} className="text-zinc-400" />
+                        <span>
+                            {formatDistanceToNow(new Date(entry.timestamp), {
+                                includeSeconds: true,
+                                addSuffix: true,
+                                locale: de,
+                            })}{" "}
+                            erstellt.
+                        </span>
+                    </div>
+
+                    {entry.variation.length > 0 && (
+                        <div className="mt-6">
+                            <h3 className="text-xl font-semibold mb-3 text-zinc-200">Variationen</h3>
+                            <div className="bg-zinc-700 p-4 rounded-xl">
+                                <ul className="list-disc ml-6 space-y-1">
+                                    {entry.variation.map((var1, index) => (
+                                        <li key={index} className="text-zinc-300">
+                                            {var1}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    )}
+
+                    {entry.categories.length > 0 && (
+                        <div className="mt-6">
+                            <h3 className="text-xl font-semibold mb-3 text-zinc-200">Kategorien</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {entry.categories.map((category, index) => (
+                                    <Link key={index} href={`/c/${category}`} passHref>
+                                        <span
+                                            className="px-3 py-1 bg-zinc-700 border-2 border-zinc-700 text-zinc-200 rounded-full text-sm cursor-pointer hover:bg-zinc-800 transition-all duration-300">
+                                            {category}
+                                        </span>
+                                    </Link>
                                 ))}
-                            </ul>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
 
-                {entry.categories.length > 0 && (
-                    <div className="mt-6">
-                        <h3 className="text-xl font-semibold mb-3 text-zinc-200">Kategorien</h3>
-                        <div className="flex flex-wrap gap-2">
-                            {entry.categories.map((category, index) => (
-                                <Link key={index} href={`/c/${category}`} passHref>
-                                    <span
-                                        className="px-3 py-1 bg-zinc-700 border-2 border-zinc-700 text-zinc-200 rounded-full text-sm cursor-pointer hover:bg-zinc-800 transition-all duration-300">
-                                        {category}
-                                    </span>
-                                </Link>
-                            ))}
-                        </div>
-                    </div>
-                )}
+                    {user && (
+                        <Link href={`/u/${user.username}`}>
+                            <div
+                                className="p-2 mt-8 w-fit flex items-center rounded-xl bg-zinc-800 hover:bg-zinc-600 border-2 border-zinc-800 transition-all transform hover:rounded-xl hover:scale-[1.04]">
+                                <img
+                                    src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`}
+                                    alt={`${user.username}'s Avatar`}
+                                    width={36}
+                                    height={36}
+                                    className="rounded-full mr-4"
+                                    loading="lazy"
+                                />
+                                <span className="text-lg font-medium">von @{user.username}</span>
+                                {user.roles?.includes("owner") && (
+                                    <BadgeCheck className="ml-1 p-1 text-red-400" size={24} aria-label="Admin Badge" />
+                                )}
+                                {user.roles?.includes("admin") && (
+                                    <HeartHandshake className="p-1 text-yellow-400" size={26} aria-label="Admin Badge" />
+                                )}
+                                {user.roles?.includes("developer") && (
+                                    <Server className="p-1 text-white" size={26} aria-label="Developer Badge" />
+                                )}
+                            </div>
+                        </Link>
+                    )}
 
-                {user && (
-                    <Link href={`/u/${user.username}`}>
-                        <div
-                            className="p-2 mt-8 w-fit flex items-center rounded-xl bg-zinc-800 hover:bg-zinc-600 border-2 border-zinc-800 transition-all transform hover:rounded-xl hover:scale-[1.04]">
-                            <img
-                                src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`}
-                                alt={`${user.username}'s Avatar`}
-                                width={36}
-                                height={36}
-                                className="rounded-full mr-4"
-                                loading="lazy"
-                            />
-                            <span className="text-lg font-medium">von @{user.username}</span>
-                            {user.roles?.includes("owner") && (
-                                <BadgeCheck className="ml-1 p-1 text-red-400" size={24} aria-label="Admin Badge" />
-                            )}
-                            {user.roles?.includes("admin") && (
-                                <HeartHandshake className="p-1 text-yellow-400" size={26} aria-label="Admin Badge" />
-                            )}
-                            {user.roles?.includes("developer") && (
-                                <Server className="p-1 text-white" size={26} aria-label="Developer Badge" />
-                            )}
+                    {relevantEntries.length > 0 && (
+                        <div className="mt-12">
+                            <h3 className="text-xl font-semibold mb-3 text-zinc-200">Verwandte Einträge</h3>
+                            <div className="flex flex-col space-y-4">
+                                {relevantEntries.map((relevantEntry) => (
+                                    <Link key={relevantEntry.id} href={`/e/${relevantEntry.id}`} passHref>
+                                        <span
+                                            className="block p-4 rounded-xl bg-zinc-700 border-2 border-zinc-700 transition-all transform hover:rounded-3xl hover:scale-[1.02] hover:bg-zinc-800">
+                                            <div>
+                                                <button
+                                                    className="text-zinc-300 text-lg font-medium">
+                                                    {relevantEntry.entry}
+                                                </button>
+                                            </div>
+                                            <p className="text-zinc-400 text-sm mt-1">
+                                                {formatDistanceToNow(new Date(relevantEntry.timestamp), {
+                                                    includeSeconds: true,
+                                                    addSuffix: true,
+                                                    locale: de,
+                                                })}
+                                            </p>
+                                        </span>
+                                    </Link>
+                                ))}
+                            </div>
                         </div>
-                    </Link>
-                )}
-
-                {relevantEntries.length > 0 && (
-                    <div className="mt-12">
-                        <h3 className="text-xl font-semibold mb-3 text-zinc-200">Relevante Einträge</h3>
-                        <div className="flex flex-col space-y-4">
-                            {relevantEntries.map((relevantEntry) => (
-                                <Link key={relevantEntry.id} href={`/e/${relevantEntry.id}`} passHref>
-                                    <span
-                                        className="block p-4 rounded-xl bg-zinc-700 border-2 border-zinc-700 transition-all transform hover:rounded-3xl hover:scale-[1.02] hover:bg-zinc-800">
-                                        <div>
-                                            <button
-                                                className="text-zinc-300 text-lg font-medium">
-                                                {relevantEntry.entry}
-                                            </button>
-                                        </div>
-                                        <p className="text-zinc-400 text-sm mt-1">
-                                            {formatDistanceToNow(new Date(relevantEntry.timestamp), {
-                                                includeSeconds: true,
-                                                addSuffix: true,
-                                                locale: de,
-                                            })}
-                                        </p>
-                                    </span>
-                                </Link>
-                            ))}
-                        </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 

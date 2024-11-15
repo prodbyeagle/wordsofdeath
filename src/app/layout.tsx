64321be1727e7397b17c12b68c118bb66185eb49
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-15 15:50:42 +0100

    - kinda seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
-
 import "./globals.css";
 import Navbar from '../components/Navbar';
 
 //! UNCOMMENT THIS WHEN COMMITING...
-// import { Inter_Tight } from 'next/font/google'
-// const sg = Inter_Tight({ fallback: ["inter-tight, system-ui"], subsets: ['latin'] })
+import { Inter_Tight } from 'next/font/google'
+const sg = Inter_Tight({ fallback: ["inter-tight, system-ui"], subsets: ['latin'] })
 
 export const metadata = {
   icons: {
     icon: '/skull.webp',
   },
-}
+  title: "WordsOfDeath",
+  description: "Words of Death. Nothing more...",
+  openGraph: {
+    title: "WordsOfDeath",
+    description: "Words of Death. Nothing more...",
+    url: "https://wordsofdeath.vercel.app/",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -19,8 +25,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {/* className={sg.className} */}
+      <head>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.openGraph?.title} />
+        <meta property="og:description" content={metadata.openGraph?.description} />
+        <meta property="og:url" content={metadata.openGraph?.url} />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href={metadata.icons.icon} />
+      </head>
+      <body className={sg.className}>
         <Navbar />
         {children}
       </body>

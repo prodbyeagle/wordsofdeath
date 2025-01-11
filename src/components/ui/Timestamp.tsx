e570c5bd2995948c70eab5ecdd6b46f6298bbd9c
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-01-12 00:33:21 +0100

    live update whooo

--- src/components/ui/Timestamp.tsx
+++ src/components/ui/Timestamp.tsx
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from "date-fns";
 import { de } from "date-fns/locale";
 import { Clock } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface TimeStampProps {
     timestamp: string;
@@ -8,6 +9,7 @@ interface TimeStampProps {
     showIcon?: boolean;
     extended?: boolean;
     text?: string;
+    live?: boolean;
 }
 
 export const TimeStamp = ({
@@ -15,19 +17,36 @@ export const TimeStamp = ({
     className,
     showIcon = true,
     extended = false,
-    text
+    text,
+    live = false
 }: TimeStampProps) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        if (live) {
+            const interval = setInterval(() => {
+                setCurrentTime(new Date());
+            }, 1000);
+
+            return () => clearInterval(interval);
+        }
+    }, [live]);
+
     return (
         <time
             dateTime={timestamp}
             className={`flex items-center gap-1 text-sm text-neutral-400 ${className}`}
         >
-            {showIcon && <Clock className="w-3.5 h-3.5" />}
+            {showIcon && <Clock size={16} />}
             {formatDistanceToNow(new Date(timestamp), {
                 includeSeconds: true,
                 addSuffix: true,
                 locale: de
             })}
+            {live && (
+                <span className="text-sm text-neutral-400"></span>
+            )}
             {extended && text && (
                 <span className="text-sm text-neutral-400">{text}</span>
             )}

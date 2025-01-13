f71e96a2bead505686f6fb7c804a135e6d32519e
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-01-13 15:00:53 +0100

    renamed modal to dialog

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -1,51 +1,50 @@
 import React, { useEffect, useState, ReactNode } from 'react';
 import { X } from 'lucide-react';
-import '../../styles/Modal.css';
 
 /**
- * Props for the Modal component, which controls the visibility, content, and styling of a modal dialog.
+ * Props for the Dialog component, which controls the visibility, content, and styling of a dialog dialog.
  */
-interface ModalProps {
+interface DialogProps {
    /**
-    * A boolean that determines if the modal is open.
+    * A boolean that determines if the dialog is open.
     * @default false
     */
    isOpen: boolean;
 
    /**
-    * A function to close the modal when triggered (e.g., by clicking the close button).
+    * A function to close the dialog when triggered (e.g., by clicking the close button).
     */
    onClose: () => void;
 
    /**
-    * The title of the modal, displayed at the top of the modal content.
+    * The title of the dialog, displayed at the top of the dialog content.
     */
    title?: string;
 
    /**
-    * The content displayed inside the modal.
+    * The content displayed inside the dialog.
     */
    children: ReactNode;
 
    /**
-    * Additional custom classes for the modal container.
+    * Additional custom classes for the dialog container.
     */
    className?: string;
 
    /**
-    * The size of the modal shadow. This determines the intensity of the shadow effect around the modal.
+    * The size of the dialog shadow. This determines the intensity of the shadow effect around the dialog.
     */
    shadowSize?: string;
 }
 
 /**
- * A Modal component that provides a flexible dialog box with customizable content, title, and styling.
- * Supports opening and closing the modal with animations, and allows closing the modal via the Escape key or close button.
+ * A Dialog component that provides a flexible dialog box with customizable content, title, and styling.
+ * Supports opening and closing the dialog with animations, and allows closing the dialog via the Escape key or close button.
  *
- * @param {ModalProps} props - The properties for the Modal component, including visibility, title, and content.
- * @returns {JSX.Element | null} A modal dialog, or null if the modal is not open.
+ * @param {DialogProps} props - The properties for the Dialog component, including visibility, title, and content.
+ * @returns {JSX.Element | null} A dialog, or null if the dialog is not open.
  */
-export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children, className, shadowSize }) => {
+export const Dialog: React.FC<DialogProps> = ({ isOpen, onClose, title, children, className, shadowSize }) => {
    const [show, setShow] = useState(isOpen);
 
    useEffect(() => {
@@ -73,15 +72,11 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children, 
 
    return (
       <div
-         aria-hidden={!isOpen}
-         aria-modal="true"
-         role="dialog"
-         className={`fixed inset-0 px-2 flex items-center justify-center bg-neutral-950 bg-opacity-60 backdrop-blur-xl z-50 ${isOpen ? 'modal-enter' : 'modal-exit'}`}
+         className={`fixed inset-0 px-2 flex items-center justify-center bg-neutral-950 bg-opacity-60 backdrop-blur-xl z-50 ${isOpen ? 'dialog-enter' : 'dialog-exit'}`}
       >
          <div className={`p-4 rounded-xl ${shadowClass} bg-neutral-900 w-fit relative transition-all duration-100 max-h-150 overflow-y-scroll border border-neutral-800 max-w-200 ${className}`}>
             <button
                onClick={onClose}
-               aria-label="Close modal"
                className="absolute top-5 right-5 text-neutral-100 p-0 bg-transparent hover:bg-neutral-800 hover:scale-105 transition-all duration-200 rounded"
             >
                <X size={24} />
